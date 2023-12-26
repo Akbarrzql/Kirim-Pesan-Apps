522d782e9446406ef1e695e5f6fc38ba3add5ee8
Akbar Rizqullah Putra Susanto <akbarrizqullah228@gmail.com>
2023-12-26 11:17:13 +0700

    style [AR]: create UI menu

--- app/src/main/java/com/example/kirimpesanapp/view/fragment/MenuFragment.kt
+++ app/src/main/java/com/example/kirimpesanapp/view/fragment/MenuFragment.kt
@@ -1,20 +1,44 @@
 package com.example.kirimpesanapp.view.fragment
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.kirimpesanapp.R
+import com.example.kirimpesanapp.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        _binding  = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUI()
+    }
+
+    private fun setUI() {
+        val isNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        if (isNightMode){
+            binding.apply {
+                clMenu.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.dark))
+                ivInfo.setImageResource(R.drawable.head_menu_dark)
+                flCamera.setBackgroundResource(R.drawable.bg_cardview_dark)
+                flGalery.setBackgroundResource(R.drawable.bg_cardview_dark)
+            }
+        }
+    }
 }
